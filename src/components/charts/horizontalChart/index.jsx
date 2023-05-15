0b815156cf56d65af3856a728824b1be720f47fa
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-05-15 18:20:18 +0600

    horizontal chart added

--- src/components/charts/horizontalChart/index.jsx
+++ src/components/charts/horizontalChart/index.jsx
@@ -1,50 +1,109 @@
-import React, { PureComponent } from "react";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
+import React from "react";
+import ReactApexChart from "react-apexcharts";
 
-const data = [
-  {
-    name: "jan 22",
-    a: 1000,
-    b: 10,
-  },
-  {
-    name: "feb 22",
-    a: 210,
-    b: 100,
-  },
-];
+class ApexChart extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default class HorizontalChart extends PureComponent {
-  static demoUrl = "https://codesandbox.io/s/stacked-bar-chart-s47i2";
+    this.state = {
+      series: [
+        {
+          name: "Mutual Funds",
+        //   21 random data
+            data: [44, 55, 41, 67, 22, 43, 21, 49, 45, 50, 56, 61, 58, 63, 60, 66, 70, 72, 75, 80, 85],
+        },
+        {
+            name: "Stocks",
+            data: [13, 23, 20, 8, 13, 27, 33, 31, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56],
+        },
+        {
+            name: "Bonds",
+            data: [11, 17, 15, 15, 21, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52],
+        },
+        {
+            name: "Cash",
+            data: [21, 7, 5, 5, 11, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40],
+        },
+        {
+            name: "Other",
+            // 21 random data
+            data: [21, 7, 5, 5, 11, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40],
+        }
+      ],
+      options: {
+        chart: {
+          type: "bar",
+          height: 850,
+          stacked: true,
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+            dataLabels: {
+              total: {
+                enabled: false,
+                offsetX: 0,
+                style: {
+                  fontSize: "13px",
+                  fontWeight: 900,
+                },
+              },
+            },
+          },
+        },
+        stroke: {
+          width: 0,
+          colors: ["#fff"],
+        },
+        title: {
+          text: "Fiction Books Sales",
+        },
+        xaxis: {
+          categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
+          colors: ["#fff"],
+          labels: {
+            formatter: function (val) {
+              return val + "K";
+            },
+          },
+        },
+        yaxis: {
+          title: {
+            text: undefined,
+          },
+          reversed: true
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return val + "K";
+            },
+          },
+        },
+        fill: {
+          opacity: 1,
+        },
+        legend: {
+          position: "top",
+          horizontalAlign: "left",
+          offsetX: 40,
+        },
+      },
+    };
+  }
 
   render() {
     return (
-      <ResponsiveContainer className="h-400" width="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-          layout="vertical"
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip cursor={{ fill: "transparent" }} />
-          <Legend />
-          <Bar dataKey="a" barSize={43} stackId="a" fill="#7F7F7F" />
-          <Bar dataKey="b" barSize={43} stackId="a" fill="#84B547" />
-        </BarChart>
-      </ResponsiveContainer>
+      <div id="chart" className="bg-[#000000] min-h-full text-black">
+        <ReactApexChart
+          options={this.state.options}
+          series={this.state.series}
+          type="bar"
+          height={870}
+        />
+      </div>
     );
   }
 }
+
+export default ApexChart;
--- src/components/charts/index.jsx
+++ src/components/charts/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import StackedBarChart from "./stackedBarChart";
 import StackedBarChart2 from "./stackedBarChart2";
-import HorizontalChart from "./horizontalChart";
+import ApexChart from "./horizontalChart";
 
 const Charts = () => {
   return (
     <div className="p-4 text-white">
-      <div className="lg:flex justify-between items-center gap-4">
+      <div className="lg:flex justify-between items-start gap-4">
         <div className="lg:w-1/2 w-full txt-white">
           <div className="bg-black rouned-lg p-4 mb-5">
             <StackedBarChart />
@@ -16,7 +16,7 @@ const Charts = () => {
           </div>
         </div>
         <div className="lg:w-1/2 w-full text-white">
-            <HorizontalChart/>
+            <ApexChart/>
         </div>
       </div>
     </div>
